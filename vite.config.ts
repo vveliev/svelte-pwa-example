188db948b18f89a018adda8c4b269e63e8cb0e46
Vagif Veliev <velievv@gmail.com>
2025-02-08 16:19:08 -0500

    chore: initial svelte setup

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import tailwindcss from '@tailwindcss/vite';
+import { paraglide } from '@inlang/paraglide-sveltekit/vite';
+import { defineConfig } from 'vitest/config';
+import { sveltekit } from '@sveltejs/kit/vite';
+
+export default defineConfig({
+	plugins: [
+		sveltekit(),
+		paraglide({
+			project: './project.inlang',
+			outdir: './src/lib/paraglide'
+		}),
+		tailwindcss()
+	],
+
+	test: {
+		include: ['src/**/*.{test,spec}.{js,ts}']
+	}
+});
