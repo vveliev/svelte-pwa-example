01af648c1874022c78a41f86c0b59dbb4580bc28
Vagif Veliev <velievv@gmail.com>
2025-02-08 22:00:03 -0500

    feat: adding translations

--- src/routes/onboarding/+page.svelte
+++ src/routes/onboarding/+page.svelte
@@ -4,6 +4,8 @@
 	import { user } from '../../stores/user';
 	import { goto } from '$app/navigation';
 	import { get } from 'svelte/store';
+	import { t, locale } from '$lib/i18n';
+	import { onMount } from 'svelte';
 
 	let userName = '';
 	let ageCategory = '';
@@ -16,6 +18,7 @@
 	let newFriendAge = '';
 	let lookUpTo = '';
 	let conflictWith = '';
+	let loaded = false;
 
 	$: {
 		if (age < 13) {
@@ -26,6 +29,12 @@
 			ageCategory = 'Adult';
 		}
 	}
+
+	onMount(() => {
+		locale.subscribe(() => {
+			loaded = true;
+		});
+	});
 
 	function saveUserInfo() {
 		user.set({ name: userName, age, ageCategory });
@@ -65,77 +74,81 @@
 	}
 </script>
 
-{#if $onboardingStep === 1}
-	<div class="onboarding-content max-w-lg mx-auto p-4 bg-white shadow-md rounded-lg">
-		<h1 class="text-2xl font-bold mb-4">User Information</h1>
-		<div class="mb-4">
-				<label for="userName" class="block text-sm font-medium text-gray-700 mb-1">Name:</label>
+{#if loaded}
+	{#if $onboardingStep === 1}
+		<div class="onboarding-content max-w-lg mx-auto p-4 bg-white shadow-md rounded-lg">
+			<h1 class="text-2xl font-bold mb-4">{$t('onboarding.user_information')}</h1>
+			<div class="mb-4">
+				<label for="userName" class="block text-sm font-medium text-gray-700 mb-1">{$t('onboarding.name')}:</label>
 				<input id="userName" type="text" bind:value={userName} class="w-full p-2 border border-gray-300 rounded-md" />
-		</div>
-		<div class="mb-4">
-				<label for="age" class="block text-sm font-medium text-gray-700 mb-1">Age:</label>
+			</div>
+			<div class="mb-4">
+				<label for="age" class="block text-sm font-medium text-gray-700 mb-1">{$t('onboarding.age')}:</label>
 				<input id="age" type="number" bind:value={age} class="w-full p-2 border border-gray-300 rounded-md" />
-		</div>
-		<div class="mb-4">
-				<label for="ageCategory" class="block text-sm font-medium text-gray-700 mb-1">Age Category:</label>
+			</div>
+			<div class="mb-4">
+				<label for="ageCategory" class="block text-sm font-medium text-gray-700 mb-1">{$t('onboarding.age_category')}:</label>
 				<p id="ageCategory" class="w-full p-2 border border-gray-300 rounded-md">{ageCategory}</p>
+			</div>
+			<div class="flex justify-between">
+				<button on:click={saveUserInfo} class="bg-blue-500 text-white px-4 py-2 rounded-md">{$t('onboarding.next')}</button>
+				<button on:click={skipOnboarding} class="bg-gray-500 text-white px-4 py-2 rounded-md">{$t('onboarding.skip')}</button>
+			</div>
 		</div>
-		<div class="flex justify-between">
-			<button on:click={saveUserInfo} class="bg-blue-500 text-white px-4 py-2 rounded-md">Next</button>
-			<button on:click={skipOnboarding} class="bg-gray-500 text-white px-4 py-2 rounded-md">Skip</button>
-		</div>
-	</div>
-{:else if $onboardingStep === 2}
-	<div class="onboarding-content max-w-lg mx-auto p-4 bg-white shadow-md rounded-lg">
-		<h1 class="text-2xl font-bold mb-4">Add Siblings</h1>
-		<div class="mb-4">
-				<label for="newSiblingName" class="block text-sm font-medium text-gray-700 mb-1">Name:</label>
+	{:else if $onboardingStep === 2}
+		<div class="onboarding-content max-w-lg mx-auto p-4 bg-white shadow-md rounded-lg">
+			<h1 class="text-2xl font-bold mb-4">{$t('onboarding.add_siblings')}</h1>
+			<div class="mb-4">
+				<label for="newSiblingName" class="block text-sm font-medium text-gray-700 mb-1">{$t('onboarding.name')}:</label>
 				<input id="newSiblingName" type="text" bind:value={newSiblingName} class="w-full p-2 border border-gray-300 rounded-md" />
-		</div>
-		<div class="mb-4">
-				<label for="newSiblingAge" class="block text-sm font-medium text-gray-700 mb-1">Age:</label>
+			</div>
+			<div class="mb-4">
+				<label for="newSiblingAge" class="block text-sm font-medium text-gray-700 mb-1">{$t('onboarding.age')}:</label>
 				<input id="newSiblingAge" type="number" bind:value={newSiblingAge} class="w-full p-2 border border-gray-300 rounded-md" />
-		</div>
-		<button on:click={addSibling} class="bg-green-500 text-white px-4 py-2 rounded-md mt-2">Add Sibling</button>
-		<ul class="list-disc pl-5 mt-4">
-			{#each siblings as sibling}
-				<li>{sibling.name} ({sibling.age} years old)</li>
-			{/each}
-		</ul>
-		<h1 class="text-2xl font-bold mb-4 mt-8">Add Friends</h1>
-		<div class="mb-4">
-				<label for="newFriendName" class="block text-sm font-medium text-gray-700 mb-1">Name:</label>
+			</div>
+			<button on:click={addSibling} class="bg-green-500 text-white px-4 py-2 rounded-md mt-2">{$t('onboarding.add_sibling')}</button>
+			<ul class="list-disc pl-5 mt-4">
+				{#each siblings as sibling}
+					<li>{sibling.name} ({sibling.age} years old)</li>
+				{/each}
+			</ul>
+			<h1 class="text-2xl font-bold mb-4 mt-8">{$t('onboarding.add_friends')}</h1>
+			<div class="mb-4">
+				<label for="newFriendName" class="block text-sm font-medium text-gray-700 mb-1">{$t('onboarding.name')}:</label>
 				<input id="newFriendName" type="text" bind:value={newFriendName} class="w-full p-2 border border-gray-300 rounded-md" />
-		</div>
-		<div class="mb-4">
-				<label for="newFriendAge" class="block text-sm font-medium text-gray-700 mb-1">Age:</label>
+			</div>
+			<div class="mb-4">
+				<label for="newFriendAge" class="block text-sm font-medium text-gray-700 mb-1">{$t('onboarding.age')}:</label>
 				<input id="newFriendAge" type="number" bind:value={newFriendAge} class="w-full p-2 border border-gray-300 rounded-md" />
+			</div>
+			<button on:click={addFriend} class="bg-green-500 text-white px-4 py-2 rounded-md mt-2">{$t('onboarding.add_friend')}</button>
+			<ul class="list-disc pl-5 mt-4">
+				{#each friends as friend}
+					<li>{friend.name} ({friend.age} years old)</li>
+				{/each}
+			</ul>
+			<div class="flex justify-between mt-4">
+				<button on:click={saveSiblingsAndFriends} class="bg-blue-500 text-white px-4 py-2 rounded-md">{$t('onboarding.next')}</button>
+				<button on:click={skipOnboarding} class="bg-gray-500 text-white px-4 py-2 rounded-md">{$t('onboarding.skip')}</button>
+			</div>
 		</div>
-		<button on:click={addFriend} class="bg-green-500 text-white px-4 py-2 rounded-md mt-2">Add Friend</button>
-		<ul class="list-disc pl-5 mt-4">
-			{#each friends as friend}
-				<li>{friend.name} ({friend.age} years old)</li>
-			{/each}
-		</ul>
-		<div class="flex justify-between mt-4">
-			<button on:click={saveSiblingsAndFriends} class="bg-blue-500 text-white px-4 py-2 rounded-md">Next</button>
-			<button on:click={skipOnboarding} class="bg-gray-500 text-white px-4 py-2 rounded-md">Skip</button>
-		</div>
-	</div>
-{:else if $onboardingStep === 3}
-	<div class="onboarding-content max-w-lg mx-auto p-4 bg-white shadow-md rounded-lg">
-		<h1 class="text-2xl font-bold mb-4">Personalization</h1>
-		<div class="mb-4">
-				<label for="lookUpTo" class="block text-sm font-medium text-gray-700 mb-1">Someone you look up to:</label>
+	{:else if $onboardingStep === 3}
+		<div class="onboarding-content max-w-lg mx-auto p-4 bg-white shadow-md rounded-lg">
+			<h1 class="text-2xl font-bold mb-4">{$t('onboarding.personalization')}</h1>
+			<div class="mb-4">
+				<label for="lookUpTo" class="block text-sm font-medium text-gray-700 mb-1">{$t('onboarding.someone_you_look_up_to')}:</label>
 				<input id="lookUpTo" type="text" bind:value={lookUpTo} class="w-full p-2 border border-gray-300 rounded-md" />
-		</div>
-		<div class="mb-4">
-				<label for="conflictWith" class="block text-sm font-medium text-gray-700 mb-1">Someone you are in conflict with:</label>
+			</div>
+			<div class="mb-4">
+				<label for="conflictWith" class="block text-sm font-medium text-gray-700 mb-1">{$t('onboarding.someone_you_are_in_conflict_with')}:</label>
 				<input id="conflictWith" type="text" bind:value={conflictWith} class="w-full p-2 border border-gray-300 rounded-md" />
+			</div>
+			<div class="flex justify-between mt-4">
+				<button on:click={savePersonalization} class="bg-blue-500 text-white px-4 py-2 rounded-md">{$t('onboarding.submit')}</button>
+				<button on:click={skipOnboarding} class="bg-gray-500 text-white px-4 py-2 rounded-md">{$t('onboarding.skip')}</button>
+			</div>
 		</div>
-		<div class="flex justify-between mt-4">
-			<button on:click={savePersonalization} class="bg-blue-500 text-white px-4 py-2 rounded-md">Submit</button>
-			<button on:click={skipOnboarding} class="bg-gray-500 text-white px-4 py-2 rounded-md">Skip</button>
-		</div>
-	</div>
+	{/if}
+{:else}
+	<p>Loading...</p>
 {/if}
